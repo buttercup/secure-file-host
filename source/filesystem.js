4cbdbb5aed91cddc613d7322056226e7b38dce5a
Perry Mitchell <perry@perrymitchell.net>
2018-11-19 22:10:51 +0200

    Filter dot-prefixed files

--- source/filesystem.js
+++ source/filesystem.js
@@ -8,21 +8,25 @@ const readFile = pify(fs.readFile);
 const stat = pify(fs.stat);
 const writeFile = pify(fs.writeFile);
 
+const FILES_TO_HIDE = /^\./;
+
 function getDirectoryContents(dir) {
     return readDir(dir)
         .then(contents => Promise.all(contents.map(filename =>
             stat(path.join(dir, filename))
                 .then(stat => ({
                     name: filename,
+                    directory: dir,
                     isFile: stat.isFile(),
                     isDirectory: stat.isDirectory()
                 }))
         )))
         .then(contents =>
             contents
-                .filter(item => item.isDirectory || item.isFile)
+                .filter(item => (item.isDirectory || item.isFile) && FILES_TO_HIDE.test(item.name) === false)
                 .map(dirEnt => ({
                     name: dirEnt.name,
+                    directory: dirEnt.directory,
                     type: dirEnt.isDirectory ? "directory" : "file"
                 }))
         );
